f900b529dd363d2a389915b7129dec39023c9b1c
Gareth Healy <garethahealy@gmail.com>
2016-10-31 16:02:09 +0000

    Updated deploy to be a script

---
+++ .travis/deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ "${TRAVIS_PULL_REQUEST}" = "false" ]];
+then
+  mvn deploy -B -Prelease-ossrh --settings .travis/deploy-settings.xml
+fi
