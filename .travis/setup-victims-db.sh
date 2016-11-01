5fc7e822e2f51ebb7d6cb2e40eaec5a85118e6ae
Gareth Healy <garethahealy@gmail.com>
2016-11-01 13:06:21 +0000

    Updated db setup to print out db schema

--- .travis/setup-victims-db.sh
+++ .travis/setup-victims-db.sh
@@ -3,7 +3,9 @@
 echo "Setting up victims DB..."
 
 mysql -u root -e 'CREATE DATABASE IF NOT EXISTS victims;'
-mysql -u root -e "SHOW DATABASES LIKE 'victims'";
 mysql -u root -e "CREATE USER 'victims'@'localhost' IDENTIFIED BY 'victims';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'victims'@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysql -u victims -p victims -e "SHOW DATABASES LIKE 'victims';";
+mysql -u victims -p victims -e "SHOW TABLES IN victims;";
