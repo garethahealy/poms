4d63be5e84ee94a9af4ed2556ecaca659d274de4
Gareth Healy <garethahealy@gmail.com>
2016-11-01 13:42:26 +0000

    Tweaked path

--- .travis/deploy-codesigning.sh
+++ .travis/deploy-codesigning.sh
@@ -2,5 +2,5 @@
 
 echo "Deploying code signing key..."
 
-openssl aes-256-cbc -K $encrypted_51bfd52bf8f1_key -iv $encrypted_51bfd52bf8f1_iv -in codesigning.asc.enc -out codesigning.asc -d
+openssl aes-256-cbc -K $encrypted_51bfd52bf8f1_key -iv $encrypted_51bfd52bf8f1_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
 gpg --fast-import .travis/codesigning.asc
